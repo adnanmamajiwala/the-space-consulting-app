c3e120e02194ef280e7672ea65994bef8f830eb7
Adnan Mamajiwala <rawkz.star@gmail.com>
2018-11-05 19:12:06 -0600

    Fixes the navigation bar to work in mobile mode - Design updates

--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -11,13 +11,18 @@ import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core
 export class HeaderComponent implements OnInit {
 
   @ViewChild('navbar') navbar: ElementRef;
+  @ViewChild('toggleButton') toggleButton: ElementRef;
+
   isNavbarCollapsed = true;
+  sidebarVisible: boolean;
+  body: HTMLElement;
 
   constructor(private renderer: Renderer2) {
   }
 
   ngOnInit() {
     const navbar: HTMLElement = this.navbar.nativeElement;
+    this.body = this.renderer.selectRootElement('body', true);
 
     this.renderer.listen('window', 'scroll', () => {
       const number = window.scrollY;
@@ -28,5 +33,29 @@ export class HeaderComponent implements OnInit {
       }
     });
   }
+
+
+  sidebarOpen() {
+    this.body.classList.add('nav-open');
+    this.toggleButton.nativeElement.classList.add('toggled');
+    this.sidebarVisible = true;
+  }
+
+  sidebarClose() {
+    this.toggleButton.nativeElement.classList.remove('toggled');
+    this.sidebarVisible = false;
+    this.body.classList.remove('nav-open');
+  }
+
+  sidebarToggle() {
+    if (this.sidebarVisible === false) {
+      this.sidebarOpen();
+    } else {
+      this.sidebarClose();
+    }
+
+    this.isNavbarCollapsed = !this.isNavbarCollapsed;
+  }
+
 
 }
